0e971090aaea6ebd97d848acb2d40ab25e8f9792
Burks <braianviana@hotmail.com>
2024-09-09 13:28:36 -0300

    Adicionado header, footer e auto cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/user';
-import { AuthGuard } from './guards';
+import { AutoCadastroComponent, HomeComponent } from './components/user';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'registrar', component: AutoCadastroComponent },
+
   //{ path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   // outras rotas
 ];
--- src/app/components/user/auto-cadastro/auto-cadastro.component.ts
+++ src/app/components/user/auto-cadastro/auto-cadastro.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auto-cadastro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './auto-cadastro.component.html',
   styleUrl: './auto-cadastro.component.css'
 })
 export class AutoCadastroComponent {
+  // Variável para controlar o perfil selecionado
+  selectedProfile: string = 'usuario'; // Por padrão, o perfil será 'usuario'
 
+  // Função para alterar o perfil
+  selectProfile(profile: string) {
+    this.selectedProfile = profile;
+  }
 }
