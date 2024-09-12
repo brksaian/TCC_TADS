32cf473842c93b0982e65f04e53f64ea0fbc6671
Burks <braianviana@hotmail.com>
2024-09-12 13:30:21 -0300

    Adicionado login, correção do tamanho das divs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { FooterComponent, HeaderComponent } from './components/shared';
 
 @Component({
@@ -8,8 +8,16 @@ import { FooterComponent, HeaderComponent } from './components/shared';
   standalone: true,
   imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corrigido para styleUrls
 })
 export class AppComponent {
   title = 'Nota Social';
+
+  constructor(private router: Router) {}
+
+  // Verifica se a rota atual é de login ou registrar
+  shouldHideHeaderFooter(): boolean {
+    const currentRoute = this.router.url;
+    return currentRoute === '/login' || currentRoute === '/registrar';
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AutoCadastroComponent, HomeComponent } from './components/user';
+import { AutoCadastroComponent, HomeComponent, LoginComponent } from './components/user';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'registrar', component: AutoCadastroComponent },
+  { path: 'login', component: LoginComponent },
 
   //{ path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   // outras rotas
