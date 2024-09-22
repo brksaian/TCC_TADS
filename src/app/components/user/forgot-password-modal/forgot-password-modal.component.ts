c536c0bc5c1920e70be5f40e17b636c1c2ae8146
brksaian <braianviana@hotmail.com>
2024-09-21 23:32:53 -0300

    Adicionado a modal de esquici a senha e mudada a fonte do projeto

---
+++ src/app/components/user/forgot-password-modal/forgot-password-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgot-password-modal.component.html',
+  styleUrl: './forgot-password-modal.component.css'
+})
+export class ForgotPasswordModalComponent {
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Railway", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
