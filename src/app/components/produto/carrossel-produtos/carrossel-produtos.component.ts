c513cd6582c26c444d542166883e98fae3ddde06
brksaian <braianviana@hotmail.com>
2024-09-24 19:52:40 -0300

    Correção do carrossel de produtos

--- src/app/components/produto/carrossel-produtos/carrossel-produtos.component.ts
+++ src/app/components/produto/carrossel-produtos/carrossel-produtos.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import Swiper from 'swiper';
-import { Produto } from '../../../shared/interface';
 import { CardProdutoComponent } from '../card-produto';
 
 @Component({
@@ -13,59 +11,45 @@ import { CardProdutoComponent } from '../card-produto';
   styleUrls: ['./carrossel-produtos.component.css']
 })
 export class CarrosselProdutosComponent {
-  @Input() produtos!: Produto[];
+  @Input() produtos: any[] = [];
+  currentIndex = signal(0);
 
-  ngAfterViewInit() {
-    new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        // Telas muito pequenas (smartphones)
-        320: {
-          slidesPerView: 1, // Um card visível
-        },
-        // Telas pequenas (smartphones maiores ou tablets pequenos)
-        480: {
-          slidesPerView: 1, // Um card visível
-        },
-        // Telas médias (tablets)
-        640: {
-          slidesPerView: 2, // Dois cards visíveis
-        },
-        // Telas médias-grandes (tablets maiores ou laptops pequenos)
-        768: {
-          slidesPerView: 2, // Dois cards visíveis
-        },
-        // Telas grandes (laptops)
-        1024: {
-          slidesPerView: 3, // Três cards visíveis
-        },
-        // Telas muito grandes (monitores maiores)
-        1280: {
-          slidesPerView: 4, // Quatro cards visíveis
-        },
-        // Telas de desktops grandes
-        1360: {
-          slidesPerView: 4, // Quatro cards visíveis
-        },
-        // Telas de desktops muito grandes
-        1600: {
-          slidesPerView: 5, // Cinco cards visíveis
-        },
-        // Telas ultragrandes (monitores widescreen)
-        1920: {
-          slidesPerView: 6, // Seis cards visíveis
-        },
-        // Telas muito largas (monitores ultrawide ou setups multimonitor)
-        2400: {
-          slidesPerView: 7, // Sete cards visíveis
-        },
-      },
-    });
+  itemsPerPage = computed(() => {
+    const width = window.innerWidth;
+    console.log('Tamanho da tela:', width);
+
+    if (width <= 480) {
+      return 1;
+    } else if (width <= 640) {
+      return 1;
+    } else if (width <= 768) {
+      return 2;
+    } else if (width <= 1024) {
+      return 2;
+    } else if (width <= 1280) {
+      return 3;
+    } else if (width <= 1536) {
+      return 4;
+    } else if (width <= 1920) {
+      return 5;
+    } else if (width <= 2560) {
+      return 6;
+    } else {
+      return 7;
+    }
+  });
+
+
+  slide(direction: number) {
+    const maxIndex = this.produtos.length - this.itemsPerPage();
+    const nextIndex = this.currentIndex() + direction * this.itemsPerPage();
+
+    if (nextIndex < 0) {
+      this.currentIndex.set(0);
+    } else if (nextIndex > maxIndex) {
+      this.currentIndex.set(maxIndex);
+    } else {
+      this.currentIndex.set(nextIndex);
+    }
   }
-
 }
