b316aa363a910cbdc1105e7427fdde701f93df81
brksaian <braianviana@hotmail.com>
2024-09-24 21:51:38 -0300

    Adicionado a busca de produto e adicionado o json para o json serve para testar a consulta ao back

--- src/app/services/produto/produto.service.ts
+++ src/app/services/produto/produto.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
+  private apiUrl = 'https://api.exemplo.com/products';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts(queryParams: any): Observable<any> {
+    return this.http.get(this.apiUrl, { params: queryParams });
+  }
 }
