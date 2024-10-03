e6bebe1f7b9cc21ecd9d2b0646edf917ab1edf63
Burks <braianviana@hotmail.com>
2024-10-03 17:55:58 -0300

    Adicionado componentes da pagina de visualização do produto

---
+++ src/app/components/produto/produtos-similares/card-produto-similar/card-produto-similar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-produto-similar',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-produto-similar.component.html',
+  styleUrl: './card-produto-similar.component.css'
+})
+export class CardProdutoSimilarComponent {
+
+}
