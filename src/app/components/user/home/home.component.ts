15532d298b8dea1fb5c34618a5977298e3a711c5
Braian Viana <42611703+brksaian@users.noreply.github.com>
2024-10-13 10:42:37 -0300

    Update home.component.ts

--- src/app/components/user/home/home.component.ts
+++ src/app/components/user/home/home.component.ts
@@ -16,7 +16,73 @@ import { HeaderHomeComponent } from "../header-home/header-home.component";
 export class HomeComponent implements OnInit {
   produtos: Produto[] = [];
 
-    categorias: Categoria[] = [];
+  categorias: Categoria[] = [
+    {
+      nome: 'Alimentos',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Higiene',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Limpeza',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Bebidas',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Carnes',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Frios e Laticínios',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Hortifruti',
+      imagem: 'assets/images/categorias/hortifruti.jpg'
+    },
+    {
+      nome: 'Padaria e Confeitaria',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Bebê',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Petshop',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Saúde e Beleza',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Papelaria',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Eletrônicos',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Utilidades Domésticas',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Automotivo',
+      imagem: 'assets/Nota Social.jpg'
+    },
+    {
+      nome: 'Brinquedos',
+      imagem: 'assets/Nota Social.jpg'
+    }
+  ];
+  
   constructor(private produtoService: ProdutoService) {}
 
   ngOnInit(): void {
