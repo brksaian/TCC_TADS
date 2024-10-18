8c1cb0a06a00ba981193c10cdc0df9daf89403d3
Burks <braianviana@hotmail.com>
2024-10-18 17:56:34 -0300

    Adicionado header do cliente web

--- src/app/components/avaliacao/visualizar-avaliacao-consumidor/visualizar-avaliacao-consumidor.component.ts
+++ src/app/components/avaliacao/visualizar-avaliacao-consumidor/visualizar-avaliacao-consumidor.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Avaliacao } from '../../../shared/interface';
 
 @Component({
   selector: 'app-visualizar-avaliacao-consumidor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './visualizar-avaliacao-consumidor.component.html',
   styleUrl: './visualizar-avaliacao-consumidor.component.css'
 })
 export class VisualizarAvaliacaoConsumidorComponent {
-
+  @Input() avaliacao!: Avaliacao;
 }
